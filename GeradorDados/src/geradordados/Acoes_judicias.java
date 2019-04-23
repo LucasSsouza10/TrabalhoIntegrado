c81c5fc848aca6dacbd686cd1fed0a3b5b845b64
LeonardoNozaki <leonardo.nozaki@dcomp.sor.ufscar.br>
2019-04-23 11:13:52 -0300

    alteracoes no gerador de dados para gerar mais de 1 divida e acao

--- GeradorDados/src/geradordados/Acoes_judicias.java
+++ GeradorDados/src/geradordados/Acoes_judicias.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package geradordados;
 
 import Conexao.ConexaoDB;
@@ -15,63 +10,61 @@ import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.Random;
 
-/**
- *
- * @author lucca
- */
 public class Acoes_judicias {
     public static void gerarAcoesJudicias(){
         try {
-             BufferedReader lerArqCpf = new BufferedReader(new InputStreamReader(new FileInputStream("cpfs.txt"), "ISO-8859-1"));
-             BufferedReader lerArqCnpj = new BufferedReader(new InputStreamReader(new FileInputStream("gerarcnpj.txt"), "ISO-8859-1"));
-             OutputStreamWriter gravarArq = new OutputStreamWriter(new FileOutputStream("Acoes_judiciais.sql"),"UTF-8");
-             
-             
-             Random random = new Random();
-             ArrayList<String> ArrayCnpj = new ArrayList();
-             String cpf = lerArqCpf.readLine();
-             String cnpj = lerArqCnpj.readLine();
-             String ins = "";
-             String cpfAux;
-             
-             
-             while(cnpj != null){
-                 ArrayCnpj.add(cnpj);
-                 cnpj = lerArqCnpj.readLine();
-             }
-             
-             ConexaoDB con = new ConexaoDB();
-             
-             while(cpf != null){
-                 int j = random.nextInt(4);
-                 for(int k = 0; k < j; k++){
-                     
+            BufferedReader lerArqCpf = new BufferedReader(new InputStreamReader(new FileInputStream("cpfs.txt"), "ISO-8859-1"));
+            BufferedReader lerArqCnpj = new BufferedReader(new InputStreamReader(new FileInputStream("gerarcnpj.txt"), "ISO-8859-1"));
+            OutputStreamWriter gravarArq = new OutputStreamWriter(new FileOutputStream("Acoes_judiciais.sql"),"UTF-8");
+
+
+            Random random = new Random();
+            ArrayList<String> ArrayCnpj = new ArrayList();
+            String cpf = lerArqCpf.readLine();
+            String cnpj = lerArqCnpj.readLine();
+            String ins = "";
+            String cpfAux;
+
+
+            while(cnpj != null){
+                ArrayCnpj.add(cnpj);
+                cnpj = lerArqCnpj.readLine();
+            }
+
+            ConexaoDB con = new ConexaoDB();
+
+            while(cpf != null){
+                int j = random.nextInt(4);
+                int k = 0;
+                while(k < j){ 
+
                     int i = random.nextInt(ArrayCnpj.size());
-                    if(random.nextBoolean())
+                    if(random.nextBoolean()){
                         ins = "insert into acoes_judiciais values ('" + gerarProcesso() + "','" + cpf + "','" +  ArrayCnpj.get(i) + "','" +  gerarSP() + "','" + PessoaFisica.gerarData(18) + "'," + Float.toString(300 + random.nextInt(50000)) + ");";
-                    else{
-                        while((cpfAux = PessoaFisica.gerarCpf()).equals(cpf)){
-                        
-                        }
-                        ins = "insert into acoes_judiciais values ('" + gerarProcesso() + "','" + cpf + "','" +  cpfAux + "','" +  gerarSP() + "','" + PessoaFisica.gerarData(18) + "', " + Float.toString(300 + random.nextInt(50000)) + ");";
+
                     }
-                    
-                    
-                 }
-                 
-                 if(con.insere(ins)){
-                    gravarArq.write(ins + "\n");
-                    cpf = lerArqCpf.readLine();
-                 }
-             }
-             
-             lerArqCnpj.close();
-             lerArqCpf.close();
-             gravarArq.close();
-         } catch (IOException  ex) {
-             System.err.printf("Erro na abertura do arquivo: %s.\n",
-             ex.getMessage());
-         }
+                    else{
+                        while((cpfAux = PessoaFisica.gerarCpf()).equals(cpf)){ }
+                        ins = "insert into acoes_judiciais values ('" + gerarProcesso() + "','" + cpf + "','" +  cpfAux + "','" +  gerarSP() + "','" + PessoaFisica.gerarData(18) + "', " + Float.toString(300 + random.nextInt(50000)) + ");";
+
+                    }
+                    if(con.insere(ins)){
+                        gravarArq.write(ins + "\n");
+                        k++;
+                     }
+
+                }
+                cpf = lerArqCpf.readLine();
+
+            }
+
+            lerArqCnpj.close();
+            lerArqCpf.close();
+            gravarArq.close();
+        } catch (IOException  ex) {
+            System.err.printf("Erro na abertura do arquivo: %s.\n",
+            ex.getMessage());
+        }
     }
 
     private static String gerarProcesso() {
@@ -79,7 +72,12 @@ public class Acoes_judicias {
         String str = "";
         int digito;
         for (int i = 0; i < 10; i++) {
-            digito  = gerador.nextInt(10);
+            if(i == 0){
+                digito  = gerador.nextInt(9) + 1;
+            }
+            else{
+                digito  = gerador.nextInt(10);
+            }
             str = str + Integer.toString(digito);
         }
         return str;
--- GeradorDados/src/geradordados/GeradorDados.java
+++ GeradorDados/src/geradordados/GeradorDados.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package geradordados;
 
-/**
- *
- * @author lucca
- */
 public class GeradorDados {
     public static void main(String[] args){
         System.out.println("Populando tabela de Pessoa Fisica");
