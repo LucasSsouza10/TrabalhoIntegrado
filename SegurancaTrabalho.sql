8e6154c8bb0254c6a326208c26f61ebc6af177bc
LucasSsouza10 <luccas_ssouza@hotmail.com>
2019-06-07 23:21:20 -0300

    usuario e store procedure dos detalhes consulta2

---
+++ SegurancaTrabalho.sql
@@ -0,0 +1,9 @@
+﻿SELECT current_user;
+
+CREATE ROLE funcionario WITH NOINHERIT LOGIN PASSWORD '123456';
+GRANT SELECT ON pessoa_fisica, divida, acoes_judiciais, ufs TO funcionario; 
+
+SET ROLE funcionario;
+SET ROLE postgres;
+
+DROP table divida;
---
+++ storeDetalhes.sql
@@ -0,0 +1,37 @@
+﻿-- Function: consultar_acoes(character varying)
+
+-- DROP FUNCTION consultar_acoes(character varying);
+
+CREATE OR REPLACE FUNCTION consultar_acoes(IN cpf_aux character varying)
+  RETURNS TABLE(numero_do_processo character varying, ident_autor character varying, situacao character varying, data date, valor numeric) AS
+$BODY$
+	BEGIN
+		RETURN QUERY 
+		SELECT a.numero_do_processo, a.ident_autor, a.situacao_do_processo, a.data,  a.valor_acao FROM acoes_judiciais a
+		WHERE cpf_do_reu = cpf_aux;
+	END;
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100
+  ROWS 1000;
+ALTER FUNCTION consultar_acoes(character varying)
+  OWNER TO postgres;
+-- Function: consultar_dividas(character varying)
+
+
+-- DROP FUNCTION consultar_dividas(character varying);
+
+CREATE OR REPLACE FUNCTION consultar_dividas(IN cpf_aux character varying)
+  RETURNS TABLE(contrato character varying, cnpj character varying, data date, valor numeric) AS
+$BODY$
+	BEGIN
+		RETURN QUERY 
+		SELECT divida.num_contrato, divida.cnpj, divida.data, divida.valor_da_divida FROM divida
+		WHERE cpf = cpf_aux;
+	END;
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100
+  ROWS 1000;
+ALTER FUNCTION consultar_dividas(character varying)
+  OWNER TO postgres;
